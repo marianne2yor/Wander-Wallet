d499158cce678e08f0de867472992fceb7823654
Marianne Tuyor <mariannctuyor@gmail.com>
2025-09-19 15:23:00 +0800

    removed unused imports in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,3 @@
-import Balance from './components/Balance';
-import Expenses from './components/Expenses';
-import Header from './components/Header';
-import Income from './components/Income';
-import LocName from './components/LocationAndName';
-import Transactions from './components/Transactions';
-import { useState } from 'react';
-import BasicDialog from './components/BasicDialog';
-import AddNew from './components/AddNew';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import MainPage from './pages/MainPage';
